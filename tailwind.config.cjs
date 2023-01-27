43cb0d0f17826ae1acc7807d23644968d705eca4
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2023-01-27 20:21:08 +1100

    Added Mobile Navigation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,11 @@ module.exports = {
             colors: {'Background' : "#000309", "Primary" : "#001F54"},
             transitionDelay:{'0' : "0ms"},
             screens: {'bar': '850px'},
+            spacing: {
+                'ham': '7.5px',
+                'hamCenter': '3.25rem',
+                'bar': '100vh',
+                    },
         },
 	},
 	plugins: [],
