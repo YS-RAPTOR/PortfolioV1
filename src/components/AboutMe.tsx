28c3837e9e7c17dda75dc2d665994a14c9bbef7f
YS-RAPTOR <yashan.sumanaratne@gmail.com>
2023-02-15 22:14:21 +1100

    Fixed Nav in Landscape

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -4,126 +4,109 @@ import Window from "./Window";
 import { motion } from "framer-motion";
 
 const Skills = `CheckSkills --list --name Yashan
+- Programming Languages:
+    - C++
     - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
-    - C#
+    - TypeScript
+    - Python
+    - Matlab
+    - VHDL
+    - HLSL
+- AI:
+    - Pytorch
+    - Tensorflow
+    - OpenCV
+    - OpenAI Gym
+- Web Developer:
+    - HTML
+    - CSS
+    - React
+    - NextJs
+    - Astro
+- Embedded Systems:
+    - Arduino
+    - Xilinx
+    - CPLD
+- Game Development:
+    - Unity
+- Tools:
+    - Git
+    - Github
+    - VSCode
+    - Visual Studio
+    - Jupyter Notebook
+    - WSL2
+    - Docker
 >`;
 
 const variantsContainer = {
-    hidden: {
-    },
-    visible: {
-        transition: {
-            staggerChildren: 0.1,
-        }
-    }
-}
+	hidden: {},
+	visible: {
+		transition: {
+			staggerChildren: 0.1,
+		},
+	},
+};
 
 const variant = {
-    hidden: {
-        opacity: 0,
-        y: 10,
-    },
-    visible: {
-        opacity: 1,
-        y: 0,
-        transition: {
-            duration: 0.5,
-        }
-    }
-}
+	hidden: {
+		opacity: 0,
+		y: 10,
+	},
+	visible: {
+		opacity: 1,
+		y: 0,
+		transition: {
+			duration: 0.5,
+		},
+	},
+};
 
 const AboutMe = () => {
-    return (
-        <motion.div
-            variants={variantsContainer}
-            initial="hidden"
-            whileInView="visible"
-            viewport={{ once: false, amount: 0.5 }}
-            className="flex lg:flex-row flex-col h-full w-full flex-shrink flex-grow items-center justify-center"
-        >
-            <div className="flex flex-col h-full w-5/6 lg:w-2/5 items-start justify-end sm:justify-center">
-                <div className="mx-auto font-bold text-4xl xs:text-7xl text-lime-300 lg:ml-10 text-center lg:text-left">
-                    <GlitchText text="ABOUT ME" iterations={5} entranceGlitch="Random"></GlitchText>
-                </div>
-                <motion.div variants={variant} className="text-center lg:text-left text-white lg:ml-10">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, debitis praesentium
-                    laudantium fuga explicabo itaque error nam saepe placeat at aperiam! Sunt nulla itaque odio
-                    quidem sed voluptatem enim at labore. Molestiae ducimus officia reiciendis suscipit libero
-                    deserunt tempora dolorum repellendus a qui earum, quidem sunt? Deleniti odio iure ratione?
-                </motion.div>
-            </div>
-            <motion.div
-                variants={variant}
-                className="flex h-full w-full items-center justify-center">
-                <div className="m-10 h-96 max-h-96 w-full max-w-2xl 3xl:max-w-6xl 3xl:h-[48rem] 3xl:max-h-[48rem]">
-                    <Window title="terminal.exe" titleStyling="text-">
-                        <div className="3xl:max-h-[46rem] max-h-[22.25rem] flex-grow overflow-y-scroll rounded-b-lg pt-3 backdrop-blur-lg">
-                            <div className="pl-2 text-sm font-normal 3xl:text-lg text-white">
-                                <span>{">\u00A0"}</span>
-                                <TerminalText
-                                    text={Skills}
-                                    startDelay={1}
-                                    textDelay={0.015}
-                                ></TerminalText>
-                            </div>
-                        </div>
-                    </Window>
-                </div>
-            </motion.div>
-        </motion.div>
-    );
+	return (
+		<motion.div
+			variants={variantsContainer}
+			initial="hidden"
+			whileInView="visible"
+			viewport={{ once: false, amount: 0.5 }}
+			className="flex h-full w-full flex-shrink flex-grow flex-col items-center justify-center lg:flex-row"
+		>
+			<div className="flex h-full w-5/6 flex-col items-start justify-end sm:justify-center lg:w-2/5">
+				<div className="mx-auto text-center text-4xl font-bold text-lime-300 lg:ml-10 lg:text-left xs:text-7xl">
+					<GlitchText
+						text="ABOUT ME"
+						iterations={5}
+						entranceGlitch="Random"
+					></GlitchText>
+				</div>
+				<motion.div
+					variants={variant}
+					className="text-center text-white lg:ml-10 lg:text-left"
+				>
+					dawdadadad
+				</motion.div>
+			</div>
+			<motion.div
+				variants={variant}
+				className="flex h-full w-full items-center justify-center"
+			>
+				<div className="m-10 h-96 max-h-96 w-full max-w-2xl 3xl:h-[48rem] 3xl:max-h-[48rem] 3xl:max-w-6xl">
+					<Window title="terminal.exe" titleStyling="text-">
+						<div className="max-h-[22.25rem] flex-grow overflow-y-scroll rounded-b-lg pt-3 backdrop-blur-lg 3xl:max-h-[46rem]">
+							<div className="pl-2 text-sm font-normal text-white 3xl:text-lg">
+								<span>{">\u00A0"}</span>
+								<TerminalText
+									text={Skills}
+									startDelay={1}
+									textDelay={0.015}
+								></TerminalText>
+							</div>
+						</div>
+					</Window>
+				</div>
+			</motion.div>
+		</motion.div>
+	);
 };
 
 export default AboutMe;
